36c22bb8c85506871d0ca60b66243be94d07e8c3
Sabrina Gobel <b.r.i.m.sg@gmail.com>
2023-02-09 21:09:09 -0300

    criado métodos

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -15,14 +16,13 @@ public class Main {
         curso2.setDescricao("descrição");
         curso2.setCargaHoraria(4);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("mentoria de Java");
         mentoria1.setDescricao("descrição mentoria");
         mentoria1.setData(LocalDate.now());
 
+        System.out.println(curso1);
+        System.out.println(curso2);
         System.out.println(mentoria1);
 
     }
