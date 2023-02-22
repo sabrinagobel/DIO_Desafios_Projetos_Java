32fcb472ee0bd265a39da2dd1340839f587ba51b
sabrinagobel <b.r.i.m.sg@gmail.com>
2023-02-22 14:11:18 -0300

    finished design patterns

--- design_patterns_spring/src/main/java/one/digitalinnovation/gof/DesignPatternsSpringApplication.java
+++ design_patterns_spring/src/main/java/one/digitalinnovation/gof/DesignPatternsSpringApplication.java
@@ -6,13 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-/*
- * @ComponentScan({"one.digitalinnovation.gof.controller"})
- * 
- * @EntityScan({"one.digitalinnovation.gof.model"})
- * 
- * @EnableJpaRepositories({"one.digitalinnovation.gof.repository"})
- */
+
 public class DesignPatternsSpringApplication {
 
 	public static void main(String[] args) {
--- design_patterns_spring/src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
+++ design_patterns_spring/src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
@@ -22,7 +22,6 @@ public class ClienteRestController {
 
 
 	@Autowired
-	
 	private ClienteService clienteService;
 	
 	@GetMapping
